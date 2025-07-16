460ec25a0d45484231bdded0d930607e69a8d47a
tharuka01 <tdbtharuka@gmail.com>
2025-07-16 20:06:35 +0530

    added a preloader

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+gsap.fromTo(
+  ".loading-page",
+  { opacity: 1 },
+  {
+    opacity: 0,
+    display: "none",
+    duration: 2,
+    delay: 3,
+  }
+);
+
+gsap.fromTo(
+  ".logo-name",
+  {
+    y: 50,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    delay: 0.5,
+  }
+);
